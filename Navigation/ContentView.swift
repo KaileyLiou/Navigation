e04963841fe5fc8f0b4430a6d8baa97ba1999ce1
Kailey Liou <kaileyliou09@gmail.com>
2025-08-07 10:15:42 -0400

    added a third view and home button

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -11,10 +11,25 @@ struct ContentView: View {
     var body: some View {
         
         NavigationStack {
-            Text("This is the root view 🌳")
-            NavigationLink(destination: Text("You've arrived to the Second View 🎊")) { // destination shows what will be on the new view
-                Text("Click me!") // the label content shows to the user the link
-            }
+            
+            VStack { // VStack to organize links top to bottom
+                
+                Text("This is the root view 🌳")
+                    .font(.title2)
+                
+                NavigationLink(destination: SecondView()) { // destination shows what will be on the new view
+                    Text("Click me!") // the label content shows to the user the link
+                }
+                
+                NavigationLink(destination: Text("You've arrived to the Third View 🎊")) { // destination shows what will be on the new view
+                    Text("Click me!") // the label content shows to the user the link
+                }
+                
+            } // end of VStack
+            
+            .navigationTitle("Home 🏠")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarHidden(true)
         } // end of the NavigationStack
 
     }
