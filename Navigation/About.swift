b782ccaec5bb09786bc6778d2f09f6eadc19a5c2
Kailey Liou <kaileyliou09@gmail.com>
2025-08-07 10:45:31 -0400

    added content in the views

--- Navigation/About.swift
+++ Navigation/About.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct About: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hi, I'm Kailey!")
+            .font(.largeTitle)
+            .foregroundColor(Color.pink)
+        Text("I'm a rising high school junior in NYC and a Kode With Klossy scholar!")
+            .multilineTextAlignment(.center)
     }
 }
 
